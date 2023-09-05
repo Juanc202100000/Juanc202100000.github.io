2b0c787cfee5d5b703c02c248a4073ad90bae111
Juanc202100000 <127879784+Juanc202100000@users.noreply.github.com>
2023-09-05 15:50:03 -0500

    Pagina de inicio

---
+++ Ingreso ruleta.php
@@ -0,0 +1,11 @@
+<?php
+
+include("conexion.php");
+
+if (strlen($_POST[´Nombre´]) < 1 &&
+    strlen($_POST[correo]) < 1 &&
+    strlen($_POST[telefono]) < 1 &&
+    strlen($_POST[N°factura]) < 1 &&) {
+}
+
+?>
---
+++ conexion.php
@@ -0,0 +1,5 @@
+<?php
+
+$conex = mysqli_connect("bh0biojwygfp06msicdq-mysql.services.clever-cloud.com","um12rzuisnrrg0dt","q89hR5G9Q6aYMUSF9aae","Ingreso Ruleta");
+
+?>
